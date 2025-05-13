82d23d8fe5c06767991f462ffaa96109dce01b6c
Xialu3421 <xz3421@ic.ac.uk>
2025-05-13 15:30:37 +0100

    temp

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import sys
+import pathlib
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))
+
 import numpy as np
 import pytest
 from src.config import ModelParams
